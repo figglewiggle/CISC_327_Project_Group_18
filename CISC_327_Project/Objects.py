4f0ef8084736a1791323ac7f439025ac8cd47fbe
Ethan Fighel <ethfig@gmail.com>
2023-10-17 14:15:56 -0400

    Update Objects.py

--- CISC_327_Project/Objects.py
+++ CISC_327_Project/Objects.py
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 class User:
     def __init__ (self, name, email, phone_number, password,address):
     #DFKVHFBVVFKVFWJLVNWEFNFV
     # DSLKNLKVNLKNVFLEJNVFEQLVNE
+=======
+# Objects.py
+>>>>>>> Stashed changes
