d365edda4ffaa44afde7c0d6a121d4ea452f12d7
Ethan Fighel <ethfig@gmail.com>
2023-10-17 14:20:17 -0400

    Update Objects.py

--- CISC_327_Project/Objects.py
+++ CISC_327_Project/Objects.py
@@ -1,8 +1,1 @@
-<<<<<<< Updated upstream
-class User:
-    def __init__ (self, name, email, phone_number, password,address):
-    #DFKVHFBVVFKVFWJLVNWEFNFV
-    # DSLKNLKVNLKNVFLEJNVFEQLVNE
-=======
-# Objects.py
->>>>>>> Stashed changes
+
