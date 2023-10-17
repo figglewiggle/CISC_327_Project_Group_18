eef4cc452f43c1d28c1af111538a5028055bb317
Ethan Fighel <ethfig@gmail.com>
2023-10-17 14:37:26 -0400

    Update Objects.py

--- CISC_327_Project/Objects.py
+++ CISC_327_Project/Objects.py
@@ -1,1 +1,9 @@
-
+class Restaurant:
+    def __init__(self, name, phone_number, address, cuisine_list, item_list, favourites_list):
+        self.name = name
+        self.phone_number = phone_number
+        self.address = address
+        self.cuisine_list = cuisine_list
+        self.item_list = item_list
+        self.favourites_list = favourites_list
+    
