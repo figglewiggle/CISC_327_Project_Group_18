e22dbb81fdcdff0dff4b875fe9ea4958c988c655
chanakyagajjar <chanakyagajjar64@gmail.com>
2023-10-17 15:06:58 -0400

    Update Objects.py

--- CISC_327_Project/Objects.py
+++ CISC_327_Project/Objects.py
@@ -1,4 +1,5 @@
 class User:
+<<<<<<< Updated upstream
     def __init__ (self, name, email, phone_number, password,address):
     #DFKVHFBVVFKVFWJLVNWEFNFV
     # DSLKNLKVNLKNVFLEJNVFEQLVNE
@@ -41,3 +42,63 @@ class Item:
         self.description = description
     
     
+=======
+    def __init__ (self, name, email, phone_number, password, address_list, card_number):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = password
+        self.addresses = address_list
+        self.payment_methods = card_list
+
+    def AddAddress(self):
+        address = input(str("Please enter the address you want to add:\n"))
+        self.addresses.append(address)
+    
+    def DeleteAddress(self):
+        print("\nHere are the list of addresses:\n")
+        for i in range(len(self.addresses)):
+            print(str(i+1) + ". " + self.addresses[i])
+        address_index = input(int("Which address would you like to delete? (1 - " + str(len(self.addresses) + 1) + ")\n"))
+        while address_index not in range(1, len(self.addresses) + 1):
+            print("\nInvalid input. Please enter an address index that is in the list.")
+            address_index = input(int("Which address would you like to delete? (1 - " + str(len(self.addresses) + 1) + ")\n"))
+        self.addresses.pop(address_index)
+
+    def EditAddress(self):
+        print("\nHere are the list of addresses:\n")
+        for i in range(len(self.addresses)):
+            print(str(i+1) + ". " + self.addresses[i])
+        address_index = input(int("Which address would you like to edit? (1 - " + str(len(self.addresses) + 1) + ")\n"))
+        while address_index not in range(1, len(self.addresses) + 1):
+            print("\nInvalid input. Please enter an address index that is in the list.")
+            address_index = input(int("Which address would you like to edit? (1 - " + str(len(self.addresses) + 1) + ")\n"))
+        address = input(str("Please enter the new address:\n"))
+        self.addresses[address_index] = address
+
+    def EditEmail(self):
+        email = input(str("Please enter the new email:\n"))
+        self.email = email
+    
+    def EditName(self):
+        name = input(str("Please enter the new name:\n"))
+        self.name = name     
+
+    def EditPhoneNumber(self):
+        phone_number = input(str("Please enter the new phone number:\n"))
+        self.phone_number = phone_number
+
+    def AddPaymentMethod(self):
+        card_number = input(str("Please enter the card number you want to add:\n"))
+        self.payment_methods.append(card_number)
+
+    def DeletePaymentMethod(self):
+        print("\nHere are the list of payment methods:\n")
+        for i in range(len(self.payment_methods):
+            print(str(i+1) + ". " + self.payment_methods[i])
+        card_index = input(int("Which card would you like to delete? (1 - " + str(len(self.payment_methods) + 1) + ")\n"))
+        while card_index not in range(1, len(self.payment_methods) + 1):
+            print("\nInvalid input. Please enter a card index that is in the list.")
+            card_index = input(int("Which card would you like to delete? (1 - " + str(len(self.addresses) + 1) + ")\n"))
+        self.payment_methods.pop(card_index)
+>>>>>>> Stashed changes
