0793846df4ee460c7b65faf587c8b887bc414f95
Ethan Fighel <ethfig@gmail.com>
2023-10-20 16:45:24 -0400

    Update app.py

--- CISC_327_Project/app.py
+++ CISC_327_Project/app.py
@@ -5,6 +5,7 @@ from Homepage.Homepage import homepage_blueprint
 from Profile_Page.Profile import profile_blueprint
 from Cart_Page.Cartpage import cartpage_blueprint
 from Homepage.Menu_Access.Menu_Access import menu_blueprint
+import os
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -18,4 +19,5 @@ app.register_blueprint(cartpage_blueprint)
 app.register_blueprint(menu_blueprint)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, threaded=True)
