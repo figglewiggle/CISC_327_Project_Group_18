08502da6c7e3cdbb3014ad42781303eac57542e9
Ethan Fighel <ethfig@gmail.com>
2023-10-20 16:19:17 -0400

    yeet

--- CISC_327_Project/Homepage/Menu_Access/Menu_Access.py
+++ CISC_327_Project/Homepage/Menu_Access/Menu_Access.py
@@ -1,6 +1,6 @@
 # Menu_Access.py
-from flask import Blueprint
+from flask import Blueprint, render_template
 menu_blueprint = Blueprint('menu',__name__)
-@menu_blueprint.route("homepage/<restaurant_name>/menu", methods=['GET','POST'])
+@menu_blueprint.route("/menu/<restaurant_name>", methods=['GET','POST'])
 def menu(restaurant_name):
-    return f"Menu for {restaurant_name}"
+    return render_template("menupage.html", restaurant_name = restaurant_name)
