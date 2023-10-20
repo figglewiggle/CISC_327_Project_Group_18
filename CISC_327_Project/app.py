5ae3a643c0f7d92cc953450d74e7b7440bfd998b
Ethan Fighel <ethfig@gmail.com>
2023-10-20 16:51:11 -0400

    yeet

--- CISC_327_Project/app.py
+++ CISC_327_Project/app.py
@@ -20,4 +20,4 @@ app.register_blueprint(menu_blueprint)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=port, debug=True, threaded=True)
+    app.run(host="127.0.0.0", port=port, debug=True, threaded=True)
