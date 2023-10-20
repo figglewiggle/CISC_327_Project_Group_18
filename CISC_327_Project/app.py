13275422472a6567bfae404ea9d72b4a2a38e421
Ethan Fighel <ethfig@gmail.com>
2023-10-20 16:34:44 -0400

    Update app.py

--- CISC_327_Project/app.py
+++ CISC_327_Project/app.py
@@ -18,4 +18,4 @@ app.register_blueprint(cartpage_blueprint)
 app.register_blueprint(menu_blueprint)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True threaded=True)
+    app.run(host="127.0.0.1", port=8080, debug=True, threaded=True)
