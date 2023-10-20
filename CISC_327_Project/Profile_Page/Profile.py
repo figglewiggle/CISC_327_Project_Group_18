ae561b727d99b9e7359eadf3c4c401e2983faab4
chanakyagajjar <chanakyagajjar64@gmail.com>
2023-10-20 13:49:19 -0400

    yeet

---
+++ CISC_327_Project/Profile_Page/Profile.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, render_template, request
+profile_blueprint = Blueprint('profile', __name__)
+@profile_blueprint.route("/profile", methods = ['GET', 'POST'])
+def profile():
+    return render_template(profile.html)
