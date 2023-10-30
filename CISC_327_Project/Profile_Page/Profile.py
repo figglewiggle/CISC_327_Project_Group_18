c072180204047d14d8f0bb50e928f2d684f3d93d
chanakyagajjar <chanakyagajjar64@gmail.com>
2023-10-30 14:59:44 -0400

    yeet

--- CISC_327_Project/Profile_Page/Profile.py
+++ CISC_327_Project/Profile_Page/Profile.py
@@ -1,8 +1,8 @@
-
+'''
 from flask import Blueprint, render_template
 from flask_login import current_user,login_required
 profile_blueprint = Blueprint('profile', __name__)
-@profile_blueprint.route("/profile", methods = ['GET', 'POST'])
+@profile_blueprint.route("/profile", methods = ['GET', 'POST', 'PUT'])
 @login_required
 def profile():
     if current_user.is_authenticated:
@@ -12,39 +12,52 @@ def profile():
         password = current_user.password
         addresses = current_user.addresses
         payment_methods = current_user.payment_methods
+        if request.method == 'PUT':
+            updated_phone_number = request.form['phone number']
+            updated_password = request.form['password']
+            #hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            updated_address = request.form['address']
+            current_user.phone_number = updated_phone_number
+            current_user.password = updated_password
+            current_user.address = updated_address
+            db.session.commit()
+            return redirect('/profile')
     return render_template("Profile.html",name=name,email=email,phone_number=phone_number, password=password, addresses=addresses, payment_methods=payment_methods)
-"""
+'''
 from flask import Blueprint, render_template, request, redirect
 from flask_login import current_user, login_required
+from werkzeug.security import generate_password_hash
+from models import User, db, bcrypt  # Remove the import for Address if addresses are stored as strings
 
 profile_blueprint = Blueprint('profile', __name__)
 
 @profile_blueprint.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
-    if request.method == 'POST':
-        if current_user.is_authenticated:
-            # Access the form data sent via POST request
-            updated_email = request.form.get('email')
-            updated_phone_number = request.form.get('phone')
-            updated_password = request.form.get('password')
-            
-            # Update the user's information in the database
-            # Example: (assuming you're using SQLAlchemy)
-            user = current_user  # Assuming you have a User model
-            user.email = updated_email
-            user.phone_number = updated_phone_number
-            # Update other fields as needed (e.g., password update needs hashing)
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            field_to_edit = request.form.get('field_to_edit')
 
-            # Commit the changes to the database
+            if field_to_edit == 'phone_number':
+                new_phone_number = request.form.get('phone_number')
+                current_user.phone_number = new_phone_number
+            elif field_to_edit == 'new_password':
+                new_password = request.form.get('new_password')
+                hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
+                current_user.password = hashed_password
+            # Add similar conditions for other fields as needed
+
+            # Save changes to the database
             db.session.commit()
+            return redirect('/profile')
 
-            # Redirect to the profile page or a success page
-            return redirect('/profile')  # Redirect to the profile page
+        # Fetch user details
+        name = current_user.name
+        email = current_user.email
+        phone_number = current_user.phone_number
+        addresses = current_user.addresses  # Assuming 'addresses' is a list of strings in the User model
+        payment_methods = current_user.payment_methods
 
-    # The rest of your GET method logic remains the same
-    # Fetch user data and render the profile template
-    # ...
+        return render_template("Profile.html", name=name, email=email, phone_number=phone_number,
+                               addresses=addresses, payment_methods=payment_methods)
 
-    return render_template("Profile.html", name=current_user.name, email=current_user.email, phone_number=current_user.phone_number, password=current_user.password, addresses=current_user.addresses, payment_methods=current_user.payment_methods)
-"""
