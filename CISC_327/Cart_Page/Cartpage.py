4862f1cf63e12d4ce23b11eba60d2c989311b984
saraj17 <20sj68@queensu.ca>
2023-10-31 15:54:45 -0400

    poop

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -3,8 +3,5 @@ from models import db, Item
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage", methods=['GET','POST'])
 def cartpage():
-    """cart_items = db.session.query(Item).filter(Item.in_cart==True).all()
-    names = []
-    for c in cart_items:
-        names.append(c.name)"""
-    return render_template("cartpage.html")
+    cart_items = Item.query.filter(Item.in_cart==True).all()
+    return render_template("cartpage.html", cart_items=cart_items)
