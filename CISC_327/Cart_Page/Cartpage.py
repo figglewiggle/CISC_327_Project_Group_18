0e10cca16c6c42a6e0daff0a0518523842ca34bf
saraj17 <20sj68@queensu.ca>
2023-10-31 12:54:40 -0400

    Update Cartpage.py

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, request
+from models import db, Item
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage", methods=['GET','POST'])
 def cartpage():
+    """cart_items = db.session.query(Item).filter(Item.in_cart==True).all()
+    names = []
+    for c in cart_items:
+        names.append(c.name)"""
     return render_template("cartpage.html")
