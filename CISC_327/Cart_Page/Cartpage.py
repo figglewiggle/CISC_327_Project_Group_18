bf6f00dbd2eec60ece154a274dd02dae80edb8c3
Ethan Fighel <ethfig@gmail.com>
2023-11-01 15:13:43 -0400

    yeet

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -3,13 +3,8 @@ from models import db, Item
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage", methods=['GET','POST'])
 def cartpage():
-<<<<<<< Updated upstream
-    cart_items = Item.query.filter_by(in_cart=True).all()
-    return render_template("cartpage.html", cart_items=cart_items)
-=======
     subtotal = 0
-    cart_items = Item.query.filter(Item.in_cart==True).all()
+    cart_items = Item.query.filter(in_cart=True).all()
     for c in cart_items:
         subtotal += c.price
     return render_template("cartpage.html", cart_items=cart_items, subtotal=subtotal)
->>>>>>> Stashed changes
