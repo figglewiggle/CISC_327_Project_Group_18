1c94704deb9a279b870ea55a0065172414248b74
Ethan Fighel <ethfig@gmail.com>
2023-11-01 15:57:03 -0400

    yeet

--- CISC_327/Cart_Page/Add/Add_To_Cart.py
+++ CISC_327/Cart_Page/Add/Add_To_Cart.py
@@ -1,17 +1,18 @@
 # Add_To_Cart.py
 # Author: Unbearable Solutions
 from flask import Blueprint, request, redirect, url_for, flash
-from models import Item
+from models import db, Item, Restaurant
 from flask_login import login_required
 add_to_cart_blueprint = Blueprint('add_to_cart',__name__)
-@add_to_cart_blueprint.route("/add_to_cart/<int:item_id>",methods=['POST'])
+@add_to_cart_blueprint.route("/add_to_cart/<item_id>/<restaurant_id>",methods=['POST'])
 @login_required
-def add_to_cart(item_id):
+def add_to_cart(item_id, restaurant_id):
     if request.method=='POST':
         item = Item.query.get(item_id)
         if not item:
             flash("Item Not Found.",'danger')
-            return redirect(url_for('menu.menu'))
-        item.in_cart = True
-        flash(f"{item.name} added to cart!", 'success')
-        return redirect(url_for('menu.menu'))
+        else:
+            item.in_cart = True
+            db.session.commit()
+            flash(f"{item.name} added to cart!", 'success')
+        return redirect(url_for('menu.menu',restaurant_id=restaurant_id))
