507a590c761a1a3656957dc7b2786d8c4ef880cd
Ethan Fighel <ethfig@gmail.com>
2023-11-01 02:10:38 -0400

    yeet

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -3,5 +3,5 @@ from models import db, Item
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage", methods=['GET','POST'])
 def cartpage():
-    cart_items = Item.query.filter(Item.in_cart==True).all()
+    cart_items = Item.query.filter_by(in_cart=True).all()
     return render_template("cartpage.html", cart_items=cart_items)
