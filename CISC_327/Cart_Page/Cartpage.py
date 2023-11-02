7bbb793ce71ef2fa0b215f238ee7a5777de766ee
Ethan Fighel <ethfig@gmail.com>
2023-11-01 21:05:58 -0400

    yeet

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, redirect, url_for, render_template, flash
 from models import Item, Restaurant
+from flask_login import current_user, login_required
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage/<restaurant_id>", methods=['GET','POST'])
+@login_required
 def cartpage(restaurant_id):
     subtotal = 0
     msg=""
     cart_items = Item.query.filter_by(in_cart=True).all()
     if not cart_items:
+<<<<<<< Updated upstream
         msg="You have not added any items to your cart."
     for c in cart_items:
         subtotal += c.price
@@ -14,3 +17,13 @@ def cartpage(restaurant_id):
     if not restaurant:
         flash(f'Restaurant not found','danger')
     return render_template("cartpage.html", cart_items=cart_items, subtotal=subtotal, restaurant=restaurant, msg=msg)
+=======
+        return redirect(url_for('homepage.homepage'))
+    else:
+        for c in cart_items:
+            subtotal += c.price
+        restaurant = Restaurant.query.get(restaurant_id)
+        if not restaurant:
+            flash(f'Restaurant not found','danger')
+        return render_template("cartpage.html", cart_items=cart_items, subtotal=subtotal, restaurant=restaurant)
+>>>>>>> Stashed changes
