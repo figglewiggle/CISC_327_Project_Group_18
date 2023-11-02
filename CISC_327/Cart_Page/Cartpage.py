07ba301f8ed897787b6fba07dcaa5f18b14ab4dc
saraj17 <20sj68@queensu.ca>
2023-11-01 22:46:00 -0400

    d

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -1,15 +1,14 @@
 from flask import Blueprint, render_template, flash
 from models import Item, Restaurant
+import Cart_Page.Subtotal.Subtotal as Subtotal
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage/<restaurant_id>", methods=['GET','POST'])
 def cartpage(restaurant_id):
-    subtotal = 0
+    subtotal = Subtotal.subtotal()
     msg=""
     cart_items = Item.query.filter_by(in_cart=True).all()
     if not cart_items:
         msg="You have not added any items to your cart."
-    for c in cart_items:
-        subtotal += c.price
     restaurant = Restaurant.query.get(restaurant_id)
     if not restaurant:
         flash(f'Restaurant not found','danger')
--- CISC_327/Cart_Page/Subtotal/Subtotal.py
+++ CISC_327/Cart_Page/Subtotal/Subtotal.py
@@ -1,1 +1,8 @@
+from models import Item
 
+def subtotal():
+    subtotal = 0
+    cart_items = Item.query.filter_by(in_cart=True).all()
+    for c in cart_items:
+        subtotal += c.price
+    return subtotal
