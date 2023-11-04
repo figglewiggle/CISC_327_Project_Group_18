1f507f860fd115f0eddb8ef09deb78a77a5e0a55
Ethan Fighel <ethfig@gmail.com>
2023-11-04 15:43:58 -0400

    yeet

--- CISC_327/Homepage/Menu_Access/Menu_Access.py
+++ CISC_327/Homepage/Menu_Access/Menu_Access.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template, flash
 from models import Restaurant
 menu_blueprint = Blueprint('menu',__name__)
 @menu_blueprint.route("/menu/<restaurant_id>", methods=['GET','POST'])
-def menu(restaurant_id):
+def menu(restaurant_id): # access menu of a given restaurant clicked on
     restaurant = Restaurant.query.get(restaurant_id)
-    if not restaurant:
+    if not restaurant: # error check if restaurant no found
         flash(f'Restaurant not found','danger')
         return render_template("homepage.html")
-    items = restaurant.item_list
+    items = restaurant.item_list 
     return render_template("menupage.html", restaurant_id=restaurant_id, restaurant_name = restaurant.name, 
                            restaurant_address=restaurant.address, restaurant_phone_number = restaurant.phone_number,
-                           restaurant_cuisine = restaurant.cuisine, items=items)
+                           restaurant_cuisine = restaurant.cuisine, items=items) # display everything about the restaurant on the menupage
