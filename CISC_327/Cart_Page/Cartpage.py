8c03fc3937f3c09d8a78e851ca7da7fa0356402a
saraj17 <20sj68@queensu.ca>
2023-11-04 14:21:32 -0400

    h

--- CISC_327/Cart_Page/Cartpage.py
+++ CISC_327/Cart_Page/Cartpage.py
@@ -5,13 +5,14 @@ from flask_login import current_user, login_required
 cartpage_blueprint = Blueprint('cartpage',__name__)
 @cartpage_blueprint.route("/cartpage/<restaurant_id>", methods=['GET','POST'])
 @login_required
+
 def cartpage(restaurant_id):
     subtotal = Subtotal.subtotal()
     msg=""
-    cart_items = Item.query.filter_by(in_cart=True).all()
+    cart_items = Item.query.filter_by(in_cart=True).all() # Gets all the items that have been added to the cart
     if not cart_items:
         msg="You have not added any items to your cart."
-        return redirect(url_for('homepage.homepage'))
+        return redirect(url_for('homepage.homepage')) # Redirects back to homepage if there are no cart items
     else:
         restaurant = Restaurant.query.get(restaurant_id)
         if not restaurant:
--- CISC_327/Checkout_Page/Checkout.py
+++ CISC_327/Checkout_Page/Checkout.py
@@ -4,44 +4,47 @@ from flask_login import current_user
 import Cart_Page.Subtotal.Subtotal as Subtotal
 checkout_blueprint = Blueprint('checkout',__name__)
 @checkout_blueprint.route("/checkout/<restaurant_id>", methods=['GET','POST'])
+
 def checkout(restaurant_id):
     subtotal = Subtotal.subtotal()
     restaurant = Restaurant.query.get(restaurant_id)
     if not restaurant:
         flash(f'Restaurant not found','danger')
-    cart_items = Item.query.filter(Item.in_cart==True).all()
+    cart_items = Item.query.filter(Item.in_cart==True).all() #Gets all the items that were in cart
     if current_user.is_authenticated:
+        # Since a user can have multiple payment methods and addresses, these are set to the default ones at first
         checkout_pm = current_user.payment_methods.filter_by(default=True).first()
         checkout_address = current_user.addresses.filter_by(default=True).first()
-        if request.method=="POST":
+        if request.method=="POST": # Checks if form was submitted
             form_data = request.form.items()
-            for key,value in form_data:
-                if key == "change_address":
-                    session['change_address'] = request.form['change_address']
+            for key,value in form_data: # Goes through the data fields that were submitted to check which button was pressed
+                if key == "change_address": # The change address button was clicked
+                    session['change_address'] = request.form['change_address'] #Stores the address they want their food delivered to 
                     new_a = request.form['change_address']
                     checkout_address = current_user.addresses.filter_by(address=new_a).first()
-                    pm_data = session.get('change_pm')  
-                    if pm_data:
+                    pm_data = session.get('change_pm') 
+                    if pm_data: # Checks to see if the user has also changed their payment method before so that it displays that one isntead of the default
                         checkout_pm = current_user.payment_methods.filter_by(card_num=pm_data).first()                
-                else:
+                else: #The change payment method button was clicked
                     session['change_pm'] = request.form['change_pm']
                     new_pm = request.form['change_pm']
                     checkout_pm = current_user.payment_methods.filter_by(card_num=new_pm).first()
                     address_data = session.get('change_address')  
                     if address_data:
                         checkout_address = current_user.addresses.filter_by(address=address_data).first()
-        
         name = current_user.name
         email = current_user.email
         phone_number = current_user.phone_number
         
+        # Gets all the addresses and payment methods to display in the dropdown menu if they want to switch
         all_addresses = current_user.addresses.all()
         all_pm = current_user.payment_methods.all()
 
+    # Calculates total price
     tax = subtotal*0.14
     total = tax + subtotal
-    tip = session.get('tip')
-    if tip:
+    tip = session.get('tip') 
+    if tip: # Checks to see if tip was added
         tip_dec = int(tip)/100
         t = total * tip_dec
         tot = total + t
