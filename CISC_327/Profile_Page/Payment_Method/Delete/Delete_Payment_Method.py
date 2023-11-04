abbd27cad9d29eb05d33aedf0df53a80e4cd121d
Ethan Fighel <ethfig@gmail.com>
2023-11-04 15:36:04 -0400

    yeet

--- CISC_327/Profile_Page/Payment_Method/Delete/Delete_Payment_Method.py
+++ CISC_327/Profile_Page/Payment_Method/Delete/Delete_Payment_Method.py
@@ -4,9 +4,9 @@ from models import db, Payment_Method
 delete_payment_blueprint = Blueprint('delete_payment',__name__)
 @delete_payment_blueprint.route('/delete_payment', methods=['POST'])
 def delete_payment():
-    payment_id = request.form.get('payment_id')
-    payment_method = Payment_Method.query.get(payment_id)
-    if payment_method.user_id == current_user.id and len(current_user.payment_methods.all())>1:
+    payment_id = request.form.get('payment_id') # get id of payment method row
+    payment_method = Payment_Method.query.get(payment_id) 
+    if payment_method.user_id == current_user.id and len(current_user.payment_methods.all())>1: # only delete payment method if there's more than one
         db.session.delete(payment_method)
         db.session.commit()
         flash('Payment Method deleted successfully!', 'success')
