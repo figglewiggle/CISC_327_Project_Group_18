10d18874acbfd2656d172b05036243d84ccf1734
Ethan Fighel <ethfig@gmail.com>
2023-11-04 13:47:30 -0400

    Update Logout.py

--- CISC_327/Sign_Up_Pages/Logout.py
+++ CISC_327/Sign_Up_Pages/Logout.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, redirect, url_for, session
+from models import db, Item
 from flask_login import logout_user, login_required
 logout_blueprint = Blueprint('logout',__name__)
 @logout_blueprint.route('/logout',methods=['GET', 'POST'])
 @login_required
 def logout():
+    items = Item.query.all()
+    for item in items:
+        item.in_cart = False
+    db.session.commit()
     session.clear()
     logout_user()
     return redirect(url_for('login.login'))
