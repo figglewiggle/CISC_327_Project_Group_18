0f3eea220fc3a885e9ee52255b7f3858d17e5168
Ethan Fighel <ethfig@gmail.com>
2023-11-13 18:47:47 -0500

    yeet

--- CISC_327/migrations/env.py
+++ CISC_327/migrations/env.py
@@ -1,6 +1,6 @@
 import logging
 from logging.config import fileConfig
-
+import os
 from flask import current_app
 
 from alembic import context
@@ -25,11 +25,18 @@ def get_engine():
 
 
 def get_engine_url():
-    try:
-        return get_engine().url.render_as_string(hide_password=False).replace(
-            '%', '%%')
-    except AttributeError:
-        return str(get_engine().url).replace('%', '%%')
+    # Check if running in a test environment
+    if os.environ.get('FLASK_ENV') == 'testing':
+        # Return the test database URL
+        test_database_url = os.environ.get('sqlite:///:memory:')
+        return test_database_url.replace('%', '%%')
+    else:
+        # Use the regular database URL
+        try:
+            return get_engine().url.render_as_string(hide_password=False).replace('%', '%%')
+        except AttributeError:
+            return str(get_engine().url).replace('%', '%%')
+
 
 
 # add your model's MetaData object here
