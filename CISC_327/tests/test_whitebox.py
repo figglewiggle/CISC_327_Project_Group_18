8086f1d17bb146cfc08db017dd1a3347daf047a6
chanakyagajjar <chanakyagajjar64@gmail.com>
2023-11-23 15:55:36 -0500

    yeet

--- CISC_327/tests/test_whitebox.py
+++ CISC_327/tests/test_whitebox.py
@@ -1,8 +1,11 @@
 #test_whitebox.py
+
 from flask import url_for
 from ..models import Item, User
+from CISC_327.Profile_Page.Edit_Phone_Number.Edit_Phone_Number import edit_phone
 
 def test_edit_phone_number(client):
-    from Edit_Phone_Number import edit_phone
+    #from Edit_Phone_Number import edit_phone
     result = edit_phone()
     assert result == url_for('profile.profile')
+
