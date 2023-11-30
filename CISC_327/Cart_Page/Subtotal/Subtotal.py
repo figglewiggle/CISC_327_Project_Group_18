9b121f0fa76c8c951a303c6edf8496d5266a4ee7
Ethan Fighel <ethfig@gmail.com>
2023-11-30 15:01:32 -0500

    yeet

--- CISC_327/Cart_Page/Subtotal/Subtotal.py
+++ CISC_327/Cart_Page/Subtotal/Subtotal.py
@@ -4,6 +4,6 @@ def subtotal():
     subtotal = 0
     cart_items = Item.query.filter_by(in_cart=True).all() # Gets all items that are in the cart
     for c in cart_items:
-        price = c.price
+        price = c.price*c.quantity
         subtotal += price # Adds all their prices 
     return subtotal
